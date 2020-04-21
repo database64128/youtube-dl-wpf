7fae73646e04430db472f6179926bbfa7aa02a28
database64128 <free122448@hotmail.com>
2020-04-21 10:57:56 +0800

    💩 Initial commit, before MVVM...

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MaterialDesignThemes.Wpf;
+
+namespace youtube_dl_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Snackbar Snackbar;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2500);
+            }).ContinueWith(t =>
+            {
+                // note you can use the message queue from any thread, but just for the demo here we 
+                // need to get the message queue from the snackbar, so need to be on the dispatcher
+                // MainSnackbar.MessageQueue.Enqueue("Welcome to Material Design In XAML Tookit");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            // DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue);
+
+            Snackbar = this.MainSnackbar;
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# 🎞⬇ Cube YouTube Downloader - `youtube-dl-wpf`
+
+A simple GUI wrapper for [`youtube-dl`](https://github.com/ytdl-org/youtube-dl).
+
+## Features
+
+- Toggle 🌃 Dark Mode and 🔆 Light Mode.
+- List available formats.
+- Override video and audio formats.
+- Prompt to update `youtube-dl`.
+- Toggle thumbnail and subtitles downloads.
+- Set custom `ffmpeg` and `youtube-dl` path.
+- Custom proxy support.
+
+## License
+
+`youtube-dl-wpf` is licensed under [GPLv3](LICENSE).
+
+[`youtube-dl`](https://github.com/ytdl-org/youtube-dl) is licensed under [The Unlicense](https://github.com/ytdl-org/youtube-dl/blob/master/LICENSE).
