database64128/youtube-dl-wpf

commit 8f17dc91b55a0e99fe9e01a29b2574674cdf59de

⛷️ Refactor backend output handling
Read from stdout and stderr streams directly, instead of registering event handlers.

YoutubeDl.Wpf/Views/HomeView.xaml.cs
@@ -172,12 +172,14 @@ namespace YoutubeDl.Wpf.Views
                 // Download, list, abort button
                 this.BindCommand(ViewModel,
                     viewModel => viewModel.StartDownloadCommand,
-                    view => view.downloadButton)
+                    view => view.downloadButton,
+                    viewModel => viewModel.Link)
                     .DisposeWith(disposables);
 
                 this.BindCommand(ViewModel,
                     viewModel => viewModel.ListFormatsCommand,
-                    view => view.listFormatsButton)
+                    view => view.listFormatsButton,
+                    viewModel => viewModel.Link)
                     .DisposeWith(disposables);
 
                 this.BindCommand(ViewModel,

YoutubeDl.Wpf/Models/BackendInstance.cs
@@ -8,7 +8,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -64,19 +63,49 @@ public class BackendInstance : ReactiveObject, IEnableLogger
         _dlProcess.StartInfo.StandardErrorEncoding = Encoding.UTF8;
         _dlProcess.StartInfo.StandardOutputEncoding = Encoding.UTF8;
         _dlProcess.EnableRaisingEvents = true;
-        _dlProcess.ErrorDataReceived += DlOutputHandler;
-        _dlProcess.OutputDataReceived += DlOutputHandler;
-        _dlProcess.Exited += DlProcess_Exited;
     }
 
-    private void DlOutputHandler(object? sendingProcess, DataReceivedEventArgs outLine)
+    private async Task RunDlAsync(CancellationToken cancellationToken = default)
     {
-        if (outLine.Data is null)
-            return;
+        if (!_dlProcess.Start())
+            throw new InvalidOperationException("Method called when the backend process is running.");
 
-        this.Log().Info(outLine.Data);
+        SetStatusRunning();
 
-        RxApp.MainThreadScheduler.Schedule(() => ParseDlOutput(outLine.Data));
+        await Task.WhenAll(
+            ReadAndParseAsync(_dlProcess.StandardError, cancellationToken),
+            ReadAndParseAsync(_dlProcess.StandardOutput, cancellationToken),
+            _dlProcess.WaitForExitAsync(cancellationToken));
+
+        SetStatusStopped();
+    }
+
+    private void SetStatusRunning()
+    {
+        StatusIndeterminate = true;
+        IsRunning = true;
+        _backendService.UpdateProgress();
+    }
+
+    private void SetStatusStopped()
+    {
+        DownloadProgressPercentage = 0.0;
+        StatusIndeterminate = false;
+        IsRunning = false;
+        _backendService.UpdateProgress();
+    }
+
+    private async Task ReadAndParseAsync(StreamReader reader, CancellationToken cancellationToken = default)
+    {
+        while (true)
+        {
+            var line = await reader.ReadLineAsync(cancellationToken);
+            if (line is null)
+                return;
+
+            this.Log().Info(line);
+            ParseDlOutput(line);
+        }
     }
 
     private void ParseDlOutput(string output)
@@ -104,20 +133,6 @@ public class BackendInstance : ReactiveObject, IEnableLogger
                 DownloadETAString = parsedStringArray[3];
             }
         }
-    }
-
-    private void DlProcess_Exited(object? sender, EventArgs e)
-    {
-        _dlProcess.CancelErrorRead();
-        _dlProcess.CancelOutputRead();
-
-        RxApp.MainThreadScheduler.Schedule(() =>
-        {
-            DownloadProgressPercentage = 0.0;
-            StatusIndeterminate = false;
-            IsRunning = false;
-            _backendService.UpdateProgress();
-        });
     }
 
     public void GenerateDownloadArguments()
@@ -231,7 +246,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
         }
     }
 
-    public void StartDownload(string link)
+    public async Task StartDownloadAsync(string link, CancellationToken cancellationToken = default)
     {
         _dlProcess.StartInfo.FileName = _settings.BackendPath;
         _dlProcess.StartInfo.ArgumentList.Clear();
@@ -242,13 +257,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
 
         try
         {
-            _dlProcess.Start();
-            _dlProcess.BeginErrorReadLine();
-            _dlProcess.BeginOutputReadLine();
-
-            StatusIndeterminate = true;
-            IsRunning = true;
-            _backendService.UpdateProgress();
+            await RunDlAsync(cancellationToken);
         }
         catch (Exception ex)
         {
@@ -256,7 +265,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
         }
     }
 
-    public void ListFormats(string link)
+    public async Task ListFormatsAsync(string link, CancellationToken cancellationToken = default)
     {
         _dlProcess.StartInfo.FileName = _settings.BackendPath;
         _dlProcess.StartInfo.ArgumentList.Clear();
@@ -271,13 +280,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
 
         try
         {
-            _dlProcess.Start();
-            _dlProcess.BeginErrorReadLine();
-            _dlProcess.BeginOutputReadLine();
-
-            StatusIndeterminate = true;
-            IsRunning = true;
-            _backendService.UpdateProgress();
+            await RunDlAsync(cancellationToken);
         }
         catch (Exception ex)
         {
@@ -285,7 +288,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
         }
     }
 
-    public async Task AbortDl(CancellationToken cancellationToken = default)
+    public async Task AbortDlAsync(CancellationToken cancellationToken = default)
     {
         if (CtrlCHelper.AttachConsole((uint)_dlProcess.Id))
         {
@@ -310,7 +313,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
         this.Log().Info("🛑 Aborted.");
     }
 
-    public void UpdateDl()
+    public async Task UpdateDlAsync(CancellationToken cancellationToken = default)
     {
         _settings.BackendLastUpdateCheck = DateTimeOffset.Now;
 
@@ -325,13 +328,7 @@ public class BackendInstance : ReactiveObject, IEnableLogger
 
         try
         {
-            _dlProcess.Start();
-            _dlProcess.BeginErrorReadLine();
-            _dlProcess.BeginOutputReadLine();
-
-            StatusIndeterminate = true;
-            IsRunning = true;
-            _backendService.UpdateProgress();
+            await RunDlAsync(cancellationToken);
         }
         catch (Exception ex)
         {

YoutubeDl.Wpf/Models/BackendService.cs
@@ -3,6 +3,8 @@ using ReactiveUI.Fody.Helpers;
 using Splat;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Shell;
 
 namespace YoutubeDl.Wpf.Models;
@@ -51,14 +53,9 @@ public class BackendService : ReactiveObject, IEnableLogger
         }
     }
 
-    public void UpdateBackend()
+    public Task UpdateBackendAsync(CancellationToken cancellationToken = default)
     {
-        var instance = Instances.Count switch
-        {
-            > 0 => Instances[0],
-            _ => new(_settings, this),
-        };
-
-        instance.UpdateDl();
+        var tasks = Instances.Select(x => x.UpdateDlAsync(cancellationToken));
+        return Task.WhenAll(tasks);
     }
 }

YoutubeDl.Wpf/ViewModels/HomeViewModel.cs
@@ -54,8 +54,8 @@ namespace YoutubeDl.Wpf.ViewModels
         public ReactiveCommand<Unit, Unit> ResetCustomFilenameTemplateCommand { get; }
         public ReactiveCommand<Unit, Unit> BrowseDownloadFolderCommand { get; }
         public ReactiveCommand<Unit, Unit> OpenDownloadFolderCommand { get; }
-        public ReactiveCommand<Unit, Unit> StartDownloadCommand { get; }
-        public ReactiveCommand<Unit, Unit> ListFormatsCommand { get; }
+        public ReactiveCommand<string, Unit> StartDownloadCommand { get; }
+        public ReactiveCommand<string, Unit> ListFormatsCommand { get; }
         public ReactiveCommand<Unit, Unit> AbortDlCommand { get; }
 
         public ReactiveCommand<Unit, Unit> OpenAddCustomPresetDialogCommand { get; }
@@ -80,6 +80,11 @@ namespace YoutubeDl.Wpf.ViewModels
                 2 => today.Day switch
                 {
                     14 => PackIconKind.Heart,
+                    _ => defaultIcon,
+                },
+                5 => today.Day switch
+                {
+                    8 => PackIconKind.Cake,
                     _ => defaultIcon,
                 },
                 10 => today.Day switch
@@ -182,9 +187,9 @@ namespace YoutubeDl.Wpf.ViewModels
             ResetCustomFilenameTemplateCommand = ReactiveCommand.Create(ResetCustomFilenameTemplate, canResetCustomFilenameTemplate);
             BrowseDownloadFolderCommand = ReactiveCommand.Create(BrowseDownloadFolder, canBrowseDownloadFolder);
             OpenDownloadFolderCommand = ReactiveCommand.Create(OpenDownloadFolder, canOpenDownloadFolder);
-            StartDownloadCommand = ReactiveCommand.Create(StartDownload, canStartDl);
-            ListFormatsCommand = ReactiveCommand.Create(ListFormats, canStartDl);
-            AbortDlCommand = ReactiveCommand.CreateFromTask(BackendInstance.AbortDl, canAbortDl);
+            StartDownloadCommand = ReactiveCommand.CreateFromTask<string>(BackendInstance.StartDownloadAsync, canStartDl);
+            ListFormatsCommand = ReactiveCommand.CreateFromTask<string>(BackendInstance.ListFormatsAsync, canStartDl);
+            AbortDlCommand = ReactiveCommand.CreateFromTask(BackendInstance.AbortDlAsync, canAbortDl);
 
             OpenAddCustomPresetDialogCommand = ReactiveCommand.Create(OpenAddCustomPresetDialog);
             OpenEditCustomPresetDialogCommand = ReactiveCommand.Create(OpenEditCustomPresetDialog, canEditOrDeletePreset);
@@ -193,7 +198,7 @@ namespace YoutubeDl.Wpf.ViewModels
 
             if (SharedSettings.BackendAutoUpdate && !string.IsNullOrEmpty(SharedSettings.BackendPath))
             {
-                BackendInstance.UpdateDl();
+                _ = BackendInstance.UpdateDlAsync();
             }
         }
 
@@ -271,16 +276,6 @@ namespace YoutubeDl.Wpf.ViewModels
         private void ResetCustomFilenameTemplate()
         {
             SharedSettings.CustomOutputTemplate = Settings.DefaultCustomFilenameTemplate;
-        }
-
-        private void StartDownload()
-        {
-            BackendInstance.StartDownload(Link);
-        }
-
-        private void ListFormats()
-        {
-            BackendInstance.ListFormats(Link);
         }
 
         private void BrowseDownloadFolder()

YoutubeDl.Wpf/ViewModels/SettingsViewModel.cs
@@ -121,7 +121,7 @@ namespace YoutubeDl.Wpf.ViewModels
 
             ChangeColorModeCommand = ReactiveCommand.Create<BaseTheme>(ChangeColorMode);
             BrowseDlBinaryCommand = ReactiveCommand.Create(BrowseDlBinary);
-            UpdateBackendCommand = ReactiveCommand.Create(_backendService.UpdateBackend, canUpdateBackend);
+            UpdateBackendCommand = ReactiveCommand.CreateFromTask(_backendService.UpdateBackendAsync, canUpdateBackend);
             BrowseFfmpegBinaryCommand = ReactiveCommand.Create(BrowseFfmpegBinary);
             OpenUri = ReactiveCommand.Create<string>(WpfHelper.OpenUri);
         }

commit 858e2c132c190ff9381a48a6abfaeadcd484066e

🎹 Fix crash when hitting enter in linkTextBox
This bug was introduced in 8f17dc91b55a0e99fe9e01a29b2574674cdf59de.

Fixes https://github.com/database64128/youtube-dl-wpf/issues/171.

YoutubeDl.Wpf/Views/HomeView.xaml.cs
@@ -2,7 +2,6 @@
 using ReactiveMarbles.ObservableEvents;
 using ReactiveUI;
 using System;
-using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Windows.Input;
@@ -30,7 +29,7 @@ namespace YoutubeDl.Wpf.Views
 
                 linkTextBox.Events().KeyDown
                            .Where(x => x.Key == Key.Enter)
-                           .Select(x => Unit.Default)
+                           .Select(_ => ViewModel!.Link)
                            .InvokeCommand(ViewModel!.StartDownloadCommand) // Null forgiving reason: upstream limitation.
                            .DisposeWith(disposables);
 