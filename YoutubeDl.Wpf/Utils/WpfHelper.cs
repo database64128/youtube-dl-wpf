0491b0da721fe444cd56baf0f29ac0793e6a7788
database64128 <free122448@hotmail.com>
2025-04-24 21:58:13 +0800

    📜 Fix auto scroll to end
    Tried to use CaretIndex and SelectionStart. Both don't work. So magic number it is.

--- YoutubeDl.Wpf/Utils/WpfHelper.cs
+++ YoutubeDl.Wpf/Utils/WpfHelper.cs
@@ -14,12 +14,15 @@ namespace YoutubeDl.Wpf.Utils
 
         /// <summary>
         /// Determines whether the TextBox is scrolled to the end.
+        /// This used to be based on https://stackoverflow.com/questions/14902394/get-the-scroll-position-of-a-wpf-textbox,
+        /// but it stopped working sometime after v1.12.2, because the calculated value became a bit off.
+        /// We now use a pre-calculated magic number. This needs to be adjusted if the font changes.
         /// </summary>
         /// <param name="textBox">The TextBox.</param>
         /// <returns>
         /// True if the TextBox is scrolled to the end.
         /// Otherwise false.
         /// </returns>
-        public static bool IsScrolledToEnd(TextBox textBox) => textBox.VerticalOffset > textBox.ExtentHeight - textBox.ViewportHeight - textBox.FontSize * textBox.FontFamily.LineSpacing;
+        public static bool IsScrolledToEnd(TextBox textBox) => textBox.VerticalOffset > textBox.ExtentHeight - textBox.ViewportHeight - 17.15;
     }
 }
