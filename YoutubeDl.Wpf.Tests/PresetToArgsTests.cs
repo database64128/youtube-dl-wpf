08cbb0f8526af4f8a747a0f08b327fc891c1c32b
database64128 <free122448@hotmail.com>
2025-09-26 02:02:52 +0800

    🛼 Fix preset tests

--- YoutubeDl.Wpf.Tests/PresetToArgsTests.cs
+++ YoutubeDl.Wpf.Tests/PresetToArgsTests.cs
@@ -6,19 +6,19 @@ namespace YoutubeDl.Wpf.Tests;
 public class PresetToArgsTests
 {
     [Theory]
-    [InlineData("", null, null, BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { })]
-    [InlineData("testName", null, null, BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { })]
-    [InlineData("testName", "248+251", null, BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "-f", "248+251", })]
+    [InlineData("", "", "", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { })]
+    [InlineData("testName", "", "", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { })]
+    [InlineData("testName", "248+251", "", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "-f", "248+251", })]
     [InlineData("testName", "248+251", "webm", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "-f", "248+251", "--merge-output-format", "webm", })]
     [InlineData("testName", "248+251", "webm", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { "-v" }, new string[] { "-f", "248+251", "--merge-output-format", "webm", "-v", })]
     [InlineData("", "248+251", "webm", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "-f", "248+251", "--merge-output-format", "webm", })]
-    [InlineData("testName", null, "webm", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "--merge-output-format", "webm", })]
+    [InlineData("testName", "", "webm", BackendTypes.Ytdl | BackendTypes.Ytdlp, new string[] { }, new string[] { "--merge-output-format", "webm", })]
     [InlineData("testName", "248+251", "webm", BackendTypes.Ytdl, new string[] { }, new string[] { "-f", "248+251", "--merge-output-format", "webm", })]
     [InlineData("testName", "248+251", "webm", BackendTypes.Ytdlp, new string[] { }, new string[] { "-f", "248+251", "--merge-output-format", "webm", })]
     public void Preset_ToArgs(
         string name,
-        string? formatArg,
-        string? containerArg,
+        string formatArg,
+        string containerArg,
         BackendTypes supportedBackends,
         string[] extraArgs,
         string[] expectedArgs)
